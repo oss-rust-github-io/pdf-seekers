a83c9cfe827acf833dec2d63b968ae7f2a5bbfbf
dlaststark <dlaststark@gmail.com>
2023-11-18 21:03:29 +0530

    moved the core logic to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use lopdf::Document as lopdoc;
 use itertools::Itertools;
 use tantivy::collector::TopDocs;
@@ -160,13 +159,12 @@ pub fn search_keyword(index: &Index, query_str: &str) -> Vec<String> {
     return matched_docs
 }
 
-pub fn run_analysis(file: &String, keyword: &str) -> HashMap<String, PDFMetadata> {
+pub fn run_analysis(file: &String, keyword: &str) -> PDFMetadata {
     let doc = lopdoc::load(&file).unwrap_or_else(|err| {
         panic!("{} - Failed to retrieve number of pages in PDF document", err);
     });
 
     let pages = doc.get_pages();
-    let mut file_metadata: HashMap<String, PDFMetadata> = HashMap::new();
     let num_pages: usize = pages.len();
     let mut matched_page_nums: Vec<u32> = Vec::new();
     let mut cropped_texts: Vec<String> = Vec::new();
@@ -192,7 +190,79 @@ pub fn run_analysis(file: &String, keyword: &str) -> HashMap<String, PDFMetadata
         }
     }
 
-    file_metadata.insert(file.clone(), PDFMetadata::new(num_pages, matched_page_nums, cropped_texts));
+    PDFMetadata::new(num_pages, matched_page_nums, cropped_texts)
+}
 
-    return file_metadata
+pub fn check_if_directory(file_or_directory: &str) -> bool {
+    let split_dir: Vec<&str> = file_or_directory.split('/').collect::<Vec<&str>>();
+
+    let last_item: &str = split_dir[split_dir.len()-1];
+    let split_item: Vec<&str> = last_item.split('.').collect::<Vec<&str>>();
+
+    if split_item.len() == 1 {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+fn get_files_in_directory(directory: &str) -> Vec<String> {
+    let mut files_list: Vec<String> = Vec::new();
+
+    let file_paths = std::fs::read_dir(directory).unwrap_or_else(|err| {
+        panic!("{} - Unable to open given directory", err);
+    });
+
+    for file in file_paths {
+        let file_name = file.unwrap().file_name().into_string().unwrap();
+        files_list.push(format!("{}/{}",directory, file_name));
+    }
+
+    files_list
+}
+
+fn file_indexing(file_path: &str, index_path: &str) {
+    // Read text in PDF file
+    let pdf_text:String = read_pdf(file_path);
+
+    // Create or open the Tantivy index
+    let index: tantivy::Index = create_or_open_index(index_path);
+
+    // Parse PDF and index content
+    parse_and_index_pdf(file_path, pdf_text, &index);
+
+    println!("{} - Indexing completed.", file_path);
+}
+
+pub fn indexing_contents(file_or_directory: &str, index_path: &str) {
+    // Check if search or indexing to be performed on single file or directory of files
+    let dir_flag: bool = check_if_directory(file_or_directory);
+
+    // Get all file names in directory
+    if dir_flag {
+        let files_list = get_files_in_directory(file_or_directory);
+
+        for file in &files_list {
+            file_indexing(&file, index_path);
+        }
+    }
+    else {
+        file_indexing(file_or_directory, index_path);
+    }
+}
+
+pub fn search_term_in_file(index_path: &str, search_term: &String) {
+    // Create or open the Tantivy index
+    let index: tantivy::Index = create_or_open_index(index_path);
+
+    // Search for a term in the indexed PDFs
+    let matched_docs: Vec<String> = search_keyword(&index, search_term);
+
+    // Run analysis on PDF documents containing the search term
+    for doc in matched_docs {
+        let metadata: PDFMetadata = run_analysis(&doc, search_term);
+        println!("File Name: {}", &doc);
+        metadata.show();
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,5 @@
-use std::collections::HashMap;
 use clap::Parser;
-use pdf_seekers::{
-    PDFMetadata,
-    read_pdf, 
-    create_or_open_index, 
-    parse_and_index_pdf, 
-    search_keyword, 
-    run_analysis
-};
+use pdf_seekers::*;
 
 #[derive(Parser, Default, Debug)]
 #[clap(author="Tapas Das <dlaststark@gmail.com>", version, about)]
@@ -60,48 +52,6 @@ fn validate_arguments(args: &Arguments) -> String {
     search_term
 }
 
-fn check_if_directory(file_or_directory: &str) -> bool {
-    let split_dir: Vec<&str> = file_or_directory.split('/').collect::<Vec<&str>>();
-
-    let last_item: &str = split_dir[split_dir.len()-1];
-    let split_item: Vec<&str> = last_item.split('.').collect::<Vec<&str>>();
-
-    if split_item.len() == 1 {
-        return true
-    }
-    else {
-        return false
-    }
-}
-
-fn file_indexing(file_path: &str, index_path: &str) {
-    // Read text in PDF file
-    let pdf_text:String = read_pdf(file_path);
-
-    // Create or open the Tantivy index
-    let index: tantivy::Index = create_or_open_index(index_path);
-
-    // Parse PDF and index content
-    parse_and_index_pdf(file_path, pdf_text, &index);
-
-    println!("{} - Indexing completed.", file_path);
-}
-
-fn search_term_in_file(index_path: &str, search_term: &String) {
-    // Create or open the Tantivy index
-    let index: tantivy::Index = create_or_open_index(index_path);
-
-    // Search for a term in the indexed PDFs
-    let matched_docs: Vec<String> = search_keyword(&index, search_term);
-
-    // Run analysis on PDF documents containing the search term
-    for doc in matched_docs {
-        let metadata: HashMap<String, PDFMetadata> = run_analysis(&doc, search_term);
-        println!("File Name: {}", &doc);
-        metadata.get(&doc).unwrap().show();
-    }
-}
-
 fn main() {
     // Read command line arguments
     let args: Arguments = Arguments::parse();
@@ -109,33 +59,12 @@ fn main() {
     // Validate command line arguments
     let search_term: String = validate_arguments(&args);
 
-    // Check if search or indexing to be performed on single file or directory of files
-    let dir_flag: bool = check_if_directory(&args.file_or_directory);
-
-    // Get all file names in directory
-    let mut files_list: Vec<String> = Vec::new();
-    if dir_flag {
-        let file_paths = std::fs::read_dir(&args.file_or_directory).unwrap_or_else(|err| {
-            panic!("{} - Unable to open given directory", err);
-        });
-
-        for file in file_paths {
-            let file_name = file.unwrap().file_name().into_string().unwrap();
-            files_list.push(format!("{}/{}",&args.file_or_directory, file_name));
-        }
-    }
-
+    // Indexing the PDF files
     if &args.action == "index" {
-        if dir_flag {
-            for file in &files_list {
-                file_indexing(&file, &args.index_path);
-            }
-        }
-        else {
-            file_indexing(&args.file_or_directory, &args.index_path);
-        }
+        indexing_contents(&args.file_or_directory, &args.index_path);
     }
 
+    // Search for provided keyword
     if &args.action == "search" {
         search_term_in_file(&args.index_path, &search_term);
     }
