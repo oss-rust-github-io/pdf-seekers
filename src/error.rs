b39bd755a79987801c2ac4bfa9b75233ed106753
dlaststark <dlaststark@gmail.com>
2023-11-19 19:02:43 +0530

    added error handling logic for keyword searching errors

--- src/error.rs
+++ src/error.rs
@@ -5,13 +5,24 @@ use chrono::prelude::Utc;
 pub enum FileOperationsError {
     FileOpenError(String, std::io::Error),
     FileReadError(String, pdf_extract::OutputError),
+    PDFFileReadError(String, lopdf::Error),
+    PDFFileTextExtractionError(String, u32, lopdf::Error),
+    DirectoryReadError(String, std::io::Error),
 }
 
 impl Display for FileOperationsError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            FileOperationsError::FileOpenError(file_name, err) => write!(f, "[{}] [ERROR] E00001 - {}: {}", Utc::now(), file_name, err),
-            FileOperationsError::FileReadError(file_name, err) => write!(f, "[{}] [ERROR] E00002 - {}: {}", Utc::now(), file_name, err),
+            FileOperationsError::FileOpenError(file_name, err) => 
+                write!(f, "[{}] [ERROR] FO0001 - {}: {}", Utc::now(), file_name, err),
+            FileOperationsError::FileReadError(file_name, err) => 
+                write!(f, "[{}] [ERROR] FO0002 - {}: {}", Utc::now(), file_name, err),
+            FileOperationsError::PDFFileReadError(file_name, err) => 
+                write!(f, "[{}] [ERROR] FO0003 - {}: {}", Utc::now(), file_name, err),
+            FileOperationsError::PDFFileTextExtractionError(file_name, page_num, err) => 
+                write!(f, "[{}] [ERROR] FO0004 - {}: Page-{} {}", Utc::now(), file_name, page_num, err),
+            FileOperationsError::DirectoryReadError(dir_path, err) => 
+                write!(f, "[{}] [ERROR] FO0005 - {}: {}", Utc::now(), dir_path, err),
         }
     }
 }
@@ -22,20 +33,61 @@ pub enum IndexingError {
     IndexDirectoryReadError(String, std::io::Error),
     IndexDirectoryCreateError(String, std::io::Error),
     IndexCreateError(String, tantivy::error::TantivyError),
-    IndexWriterCreateError(tantivy::error::TantivyError)
+    IndexWriterCreateError(tantivy::error::TantivyError),
+    IndexFieldNotFound(String, tantivy::error::TantivyError),
+    IndexDocumentAddError(tantivy::error::TantivyError),
+    IndexDocumentCommitError(tantivy::error::TantivyError),
 }
 
 impl Display for IndexingError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            IndexingError::IndexDirectoryOpenError(dir_path, err) => write!(f, "[{}] [ERROR] E00003 - {}: {}", Utc::now(), dir_path, err),
-            IndexingError::IndexDirectoryReadError(dir_path, err) => write!(f, "[{}] [ERROR] E00004 - {}: {}", Utc::now(), dir_path, err),
-            IndexingError::IndexDirectoryCreateError(dir_path, err) => write!(f, "[{}] [ERROR] E00005 - {}: {}", Utc::now(), dir_path, err),
-            IndexingError::IndexCreateError(dir_path, err) => write!(f, "[{}] [ERROR] E00006 - {}: {}", Utc::now(), dir_path, err),
-            IndexingError::IndexWriterCreateError(err) => write!(f, "[{}] [ERROR] E00007 - {}", Utc::now(), err),
+            IndexingError::IndexDirectoryOpenError(dir_path, err) => 
+                write!(f, "[{}] [ERROR] IE0001 - {}: {}", Utc::now(), dir_path, err),
+            IndexingError::IndexDirectoryReadError(dir_path, err) => 
+                write!(f, "[{}] [ERROR] IE0002 - {}: {}", Utc::now(), dir_path, err),
+            IndexingError::IndexDirectoryCreateError(dir_path, err) => 
+                write!(f, "[{}] [ERROR] IE0003 - {}: {}", Utc::now(), dir_path, err),
+            IndexingError::IndexCreateError(dir_path, err) => 
+                write!(f, "[{}] [ERROR] IE0004 - {}: {}", Utc::now(), dir_path, err),
+            IndexingError::IndexWriterCreateError(err) => 
+                write!(f, "[{}] [ERROR] IE0005 - {}", Utc::now(), err),
+            IndexingError::IndexFieldNotFound(field_name, err) => 
+                write!(f, "[{}] [ERROR] IE0006 - {}: {}", Utc::now(), field_name, err),
+            IndexingError::IndexDocumentAddError(err) => 
+                write!(f, "[{}] [ERROR] IE0007 - {}", Utc::now(), err),
+            IndexingError::IndexDocumentCommitError(err) => 
+                write!(f, "[{}] [ERROR] IE0008 - {}", Utc::now(), err),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum SearchingError {
+    IndexReaderCreateError(tantivy::error::TantivyError),
+    IndexFieldNotFound(String, tantivy::error::TantivyError),
+    QueryParserError(tantivy::query::QueryParserError),
+    KeywordSearchError(tantivy::error::TantivyError),
+    SearcherDocumentFetchError(tantivy::error::TantivyError),
+}
+
+impl Display for SearchingError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            SearchingError::IndexReaderCreateError(err) => 
+                write!(f, "[{}] [ERROR] SE0001 - {}", Utc::now(), err),
+            SearchingError::IndexFieldNotFound(field_name, err) => 
+                write!(f, "[{}] [ERROR] SE0002 - {} - {}", Utc::now(), field_name, err),
+            SearchingError::QueryParserError(err) => 
+                write!(f, "[{}] [ERROR] SE0003 - {}", Utc::now(), err),
+            SearchingError::KeywordSearchError(err) => 
+                write!(f, "[{}] [ERROR] SE0004 - {}", Utc::now(), err),
+            SearchingError::SearcherDocumentFetchError(err) => 
+                write!(f, "[{}] [ERROR] SE0005 - {}", Utc::now(), err),
         }
     }
 }
 
 impl std::error::Error for FileOperationsError {}
 impl std::error::Error for IndexingError {}
+impl std::error::Error for SearchingError {}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 pub mod error;
-use error::{FileOperationsError, IndexingError};
+use error::{FileOperationsError, IndexingError, SearchingError};
 use lopdf::Document as lopdoc;
 use itertools::Itertools;
 use tantivy::collector::TopDocs;
@@ -34,7 +34,7 @@ impl PDFMetadata {
     }
 }
 
-pub fn read_pdf(pdf_file: &str) -> Result<String, error::FileOperationsError> {
+pub fn read_pdf(pdf_file: &str) -> Result<String, FileOperationsError> {
     // Open the PDF file
     let file: Vec<u8>= match std::fs::read(pdf_file) {
         Ok(s) => s,
@@ -98,79 +98,102 @@ pub fn create_or_open_index(index_path: &str) -> Result<Index, IndexingError> {
     Ok(index)
 }
 
-pub fn parse_and_index_pdf(pdf_file: &str, pdf_text: String, index: &Index) {
+pub fn parse_and_index_pdf(pdf_file: &str, pdf_text: String, index: &Index) -> Result<(), IndexingError> {
     // Create a Tantivy index writer
-    let mut index_writer = index
-        .writer_with_num_threads(1, 40_000_000)
-        .unwrap_or_else(|err| {
-            panic!("{} - Failed to create index writer", err);
-        });
+    let mut index_writer = match index
+        .writer_with_num_threads(1, 40_000_000) {
+            Ok(s) => s,
+            Err(e) => return Err(IndexingError::IndexWriterCreateError(e))
+        };
 
     // Create a Tantivy document
     let mut doc = Document::default();
 
-    // Add PDF path to the document
-    doc.add_text(index.schema().get_field("content").unwrap(), &pdf_text);
+    // Add PDF content and path to the index document
+    doc.add_text(match index.schema().get_field("content") {
+        Ok(s) => s,
+        Err(e) => return Err(IndexingError::IndexFieldNotFound(String::from("content"), e))
+    }, &pdf_text);
+
     doc.add_text(
-        index.schema().get_field("path").unwrap(),
+        match index.schema().get_field("path") {
+            Ok(s) => s,
+            Err(e) => return Err(IndexingError::IndexFieldNotFound(String::from("path"), e))
+        },
         &pdf_file.to_string(),
     );
 
     // Add the document to the index
-    index_writer.add_document(doc).unwrap_or_else(|err| {
-        panic!("{} - Failed to add PDF content to the index", err);
-    });
+    match index_writer.add_document(doc) {
+        Ok(_) => {},
+        Err(e) => return Err(IndexingError::IndexDocumentAddError(e))
+    };
 
     // Commit changes to the index
-    index_writer.commit().unwrap_or_else(|err| {
-        panic!("{} - Failed to commit changes to the index", err);
-    });
+    match index_writer.commit() {
+        Ok(_) => {},
+        Err(e) => return Err(IndexingError::IndexDocumentCommitError(e))
+    };
+
+    Ok(())
 }
 
-pub fn search_keyword(index: &Index, query_str: &str) -> Vec<String> {
-    let indexer = index.reader().unwrap_or_else(|err| {
-        panic!("{} - Failed to create reader object for Indexer", err);
-    });
+pub fn search_keyword(index: &Index, query_str: &str) -> Result<Vec<String>, SearchingError> {
+    let indexer = match index.reader() {
+        Ok(s) => s,
+        Err(e) => return Err(SearchingError::IndexReaderCreateError(e))
+    };
+
     let searcher = indexer.searcher();
-    let schema = index.schema();
-    let query_parser = QueryParser::for_index(&index, vec![schema.get_field("content").unwrap()]);
+    let content_field = match index.schema().get_field("content") {
+        Ok(s) => s,
+        Err(e) => return Err(SearchingError::IndexFieldNotFound(String::from("content"), e))
+    };
+    let query_parser = QueryParser::for_index(&index, vec![content_field]);
 
     // Parse the query string
-    let query = query_parser.parse_query(query_str).unwrap_or_else(|err| {
-        panic!("{} - Failed to parse query for PDF document", err);
-    });
+    let query = match query_parser.parse_query(query_str) {
+        Ok(s) => s,
+        Err(e) => return Err(SearchingError::QueryParserError(e))
+    };
 
     // Search the index
-    let top_docs = searcher.search(&query, &TopDocs::with_limit(10)).unwrap_or_else(|err| {
-        panic!("{} - Failed to search keyword in PDF document", err);
-    });
-    println!("{:#?}", top_docs);
+    let top_docs = match searcher.search(&query, &TopDocs::with_limit(10)) {
+        Ok(s) => s,
+        Err(e) => return Err(SearchingError::KeywordSearchError(e))
+    };
 
     // Capture search results
     let mut matched_docs: Vec<String> = Vec::new();
     for (_score, doc_address) in top_docs {
-        let retrieved_doc = searcher.doc(doc_address).unwrap_or_else(|err| {
-            panic!("{} - Failed to retrieve PDF document", err);
-        });
-        let path_field = schema.get_field("path").unwrap();
+        let retrieved_doc = match searcher.doc(doc_address) {
+            Ok(s) => s,
+            Err(e) => return Err(SearchingError::SearcherDocumentFetchError(e))
+        };
+
+        let path_field = match index.schema().get_field("path") {
+            Ok(s) => s,
+            Err(e) => return Err(SearchingError::IndexFieldNotFound(String::from("path"), e))
+        };
+
         let path_value = retrieved_doc
             .get_first(path_field)
             .and_then(|v| v.as_text())
             .unwrap_or_default()
             .to_string();
 
-        // println!("Score: {:.2}, Path: {}", score, path_value);
-        matched_docs.push(path_value)
+        matched_docs.push(path_value);
     }
 
     let matched_docs: Vec<String> = matched_docs.into_iter().unique().collect();
-    return matched_docs
+    Ok(matched_docs)
 }
 
-pub fn run_analysis(file: &String, keyword: &str) -> PDFMetadata {
-    let doc = lopdoc::load(&file).unwrap_or_else(|err| {
-        panic!("{} - Failed to retrieve number of pages in PDF document", err);
-    });
+pub fn run_analysis(file: &String, keyword: &str) -> Result<PDFMetadata, FileOperationsError> {
+    let doc = match lopdoc::load(&file) {
+        Ok(s) => s,
+        Err(e) => return Err(FileOperationsError::PDFFileReadError(file.clone(), e))
+    };
 
     let pages = doc.get_pages();
     let num_pages: usize = pages.len();
@@ -179,9 +202,10 @@ pub fn run_analysis(file: &String, keyword: &str) -> PDFMetadata {
 
     for (i, _) in pages.iter().enumerate() {
         let page_number: u32 = (i + 1) as u32;
-        let text: String = doc.extract_text(&[page_number]).unwrap_or_else(|err| {
-            panic!("{} - Failed to retrieve text at page number {} in PDF document", err, page_number);
-        });
+        let text: String = match doc.extract_text(&[page_number]) {
+            Ok(s) => s,
+            Err(e) => return Err(FileOperationsError::PDFFileTextExtractionError(file.clone(), page_number, e))
+        };
         
         if text.contains(keyword) {
             matched_page_nums.push(page_number);
@@ -198,7 +222,7 @@ pub fn run_analysis(file: &String, keyword: &str) -> PDFMetadata {
         }
     }
 
-    PDFMetadata::new(num_pages, matched_page_nums, cropped_texts)
+    Ok(PDFMetadata::new(num_pages, matched_page_nums, cropped_texts))
 }
 
 pub fn check_if_directory(file_or_directory: &str) -> bool {
@@ -215,19 +239,20 @@ pub fn check_if_directory(file_or_directory: &str) -> bool {
     }
 }
 
-fn get_files_in_directory(directory: &str) -> Vec<String> {
+fn get_files_in_directory(directory: &str) -> Result<Vec<String>, FileOperationsError> {
     let mut files_list: Vec<String> = Vec::new();
 
-    let file_paths = std::fs::read_dir(directory).unwrap_or_else(|err| {
-        panic!("{} - Unable to open given directory", err);
-    });
+    let file_paths = match std::fs::read_dir(directory) {
+        Ok(s) => s,
+        Err(e) => return Err(FileOperationsError::DirectoryReadError(directory.to_string(), e))
+    };
 
     for file in file_paths {
         let file_name = file.unwrap().file_name().into_string().unwrap();
         files_list.push(format!("{}/{}",directory, file_name));
     }
 
-    files_list
+    Ok(files_list)
 }
 
 fn file_indexing(file_path: &str, index_path: &str) {
@@ -250,7 +275,13 @@ fn file_indexing(file_path: &str, index_path: &str) {
     };
 
     // Parse PDF and index content
-    parse_and_index_pdf(file_path, pdf_text, &index);
+    match parse_and_index_pdf(file_path, pdf_text, &index) {
+        Ok(_) => {},
+        Err(err) => {
+            eprintln!("{}", err);
+            std::process::exit(1);
+        }
+    };
 
     println!("{} - Indexing completed.", file_path);
 }
@@ -261,7 +292,13 @@ pub fn indexing_contents(file_or_directory: &str, index_path: &str) {
 
     // Get all file names in directory
     if dir_flag {
-        let files_list = get_files_in_directory(file_or_directory);
+        let files_list = match get_files_in_directory(file_or_directory) {
+            Ok(s) => s,
+            Err(err) => {
+                eprintln!("{}", err);
+                std::process::exit(1);
+            }
+        };
 
         for file in &files_list {
             file_indexing(&file, index_path);
@@ -283,11 +320,23 @@ pub fn search_term_in_file(index_path: &str, search_term: &String) {
     };
 
     // Search for a term in the indexed PDFs
-    let matched_docs: Vec<String> = search_keyword(&index, search_term);
+    let matched_docs: Vec<String> = match search_keyword(&index, search_term) {
+        Ok(s) => s,
+        Err(err) => {
+            eprintln!("{}", err);
+            std::process::exit(1);
+        }
+    };
 
     // Run analysis on PDF documents containing the search term
     for doc in matched_docs {
-        let metadata: PDFMetadata = run_analysis(&doc, search_term);
+        let metadata: PDFMetadata = match run_analysis(&doc, search_term) {
+            Ok(s) => s,
+            Err(err) => {
+                eprintln!("{}", err);
+                std::process::exit(1);
+            }
+        };
         println!("File Name: {}", &doc);
         metadata.show();
     }
